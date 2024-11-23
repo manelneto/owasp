acfd338fb6d2e47cc30248cd8cc44a4ac7671b47
Manuel Neto <manuelrlcneto@gmail.com>
2024-11-23 20:20:51 +0000

    big bang

---
+++ database.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
---
+++ profile.php
@@ -0,0 +1,37 @@
+<?php
+    require_once(__DIR__ . '/user.php');
+
+    session_start();
+
+    $db = new PDO('sqlite:' . __DIR__ . '/database.db');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $id = $_GET['id'];
+    $user = User::getUser($db, $id);
+    $username = $user->getUsername();
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Segurança de Redes 2024 &ndash; Site Vulnerável</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <h1><a href="index.php">Segurança de Redes 2024 &ndash; Site Vulnerável</a></h1>
+        </header>
+        <main>
+            <section>
+                <h2>Página Pessoal</h2>
+                <h3><?=$username?></h3>
+                <form action="logout.php" method="post">
+                    <button type="submit">Terminar Sessão</button>
+                </form>
+            </section>
+        </main>
+    </body>
+</html>
